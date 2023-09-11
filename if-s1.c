78921ee87973a62575d3d3489b25c0203d2b7f65
Adetunjieliazer <adetunjieliazer@gmail.com>
2023-09-11 03:40:01 +0100

    Programs-in-C

---
+++ if-s1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	i = 5;
+	j = 6;
+	if (i < j)
+	{
+		printf("Yes 'i' is less than 'j'.				\n");
+	} else 
+	{
+		printf("No 'i' is not greater t				han 'j'\n");
+	}
+	return 0;
+}
